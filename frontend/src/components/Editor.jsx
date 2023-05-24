a4f6feb2ba8db94e3ba0a3f196d6f75b629b3f84
vishwajeet-7 <vishady7@gmail.com>
2023-05-24 22:51:47 +0530

    updated the area to write code using codemirror

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Codemirror from 'codemirror'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/dracula.css'
 
 const Editor = () => {
+    //to display code editor
+    useEffect(()=>{
+        async function init(){
+            Codemirror.fromTextArea(document.getElementById('real_time_editor'),{
+                mode:{name:'javascript',json:true},
+                theme:'dracula',
+                autoCloseTags:true,
+                autoCloseBrackets:true,
+                lineNumbers:true,
+            });
+        }
+
+        init();
+    },[])
   return (
     <div>
-      <h1>Editor</h1>
+      <textarea id='real_time_editor'></textarea>
     </div>
   )
 }
