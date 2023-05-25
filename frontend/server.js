eefff515f58eaf5b1bc421f01ab09dd09293207b
vishwajeet-7 <vishady7@gmail.com>
2023-05-25 21:48:58 +0530

    updated joing and leaving a room functions

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,55 @@
 const express = require('express')
 const http = require('http')
 const app = express();
-const {Server} = require('socket.io')
+const {Server} = require('socket.io');
+const ACTIONS = require('./src/action');
 
 const server = http.createServer(app)
 const io = new Server(server);
 
+//to know which socket id belongs to which user
+const userSocketMap = {};
+
+const getAllConnectedClients = (roomId)=>{
+    //it is containg map
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId)=>{
+        return {
+            socketId,
+            username:userSocketMap[socketId]
+        }
+    });
+}
+
 io.on('connection',(socket)=>{
     console.log('socket connected',socket.id);
+    socket.on(ACTIONS.JOIN,({roomId,username})=>{ //getting the room id and name of client who wants to join using Action Join
+        userSocketMap[socket.id] = username; //storing user inside map
+        socket.join(roomId); //joining the room of a particular id
+        const clients = getAllConnectedClients(roomId); //list of all clients in the room
+        clients.forEach(({socketId})=>{
+            //notifying connected clients about the new joined client
+            io.to(socketId).emit(ACTIONS.JOINED,{
+                clients,
+                username, // name of user who has joined
+                socketId:socket.id, //
+            });
+        });
+    });
+
+    //if user get's disconned while using the page
+socket.on('disconnecting',()=>{
+    const rooms = [...socket.rooms]; //getting all rooms 
+    //notifying each room that the provided socket id is getting disconnected
+    rooms.forEach((roomId)=>{
+        socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+            socketId: socket.id,
+            username:userSocketMap[socket.id],
+        })
+    })
+    delete userSocketMap[socket.id]; //removing the user from map
+    socket.leave(); //method to get out of a room officially
+})
+
 });
 
 const PORT = process.env.PORT || 5000;
--- frontend/src/components/EditorPage.jsx
+++ frontend/src/components/EditorPage.jsx
@@ -1,58 +1,81 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Client from './Client'
-import Editor from './Editor'
-import { initSocket } from '../socket';
-import ACTIONS from '../action';
-import { useLocation, useParams } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import Client from "./Client";
+import Editor from "./Editor";
+import { initSocket } from "../socket";
+import ACTIONS from "../action";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const EditorPage = () => {
   //initialising socket event
   const params = useParams();
   const location = useLocation();
   const socketRef = useRef(null);
-  const roomId = params.roomID
-  useEffect(()=>{
-    const init = async ()=>{
+  const roomId = params.roomID;
+  const nav = useNavigate();
+  const [client, setClient] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
       socketRef.current = await initSocket();
-      socketRef.current.emit(ACTIONS.JOIN,{
+      //to handle connection errors
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later");
+        nav("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
-        username:location.state?.username,
-      })
-    }
+        username: location.state?.username,
+      });
+
+      //listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} has joined the room`);
+            console.log(`${username} joined`);
+          }
+          setClient(clients)
+        }
+      );
+    };
     init();
-  },[])
+  }, []);
 
-
-  const [client,setClient] = useState([
-    {socket:1,username:"Vishwajeet"},
-    {socket:2,username:"Aman"},
-    {socket:3,username:"Bingo and"}
-  ])
+  if (!location.state) return <Navigate to={"/"} />;
   return (
-    <div className='main_wrapper'>
-      <div className='aside'>
-        <div className='aside_inner'>
-          <div className='logo'>
-            <img className='logo_img' src="/mylogo.png" alt="" width={'60px'}/>
+    <div className="main_wrapper">
+      <div className="aside">
+        <div className="aside_inner">
+          <div className="logo">
+            <img className="logo_img" src="/mylogo.png" alt="" width={"60px"} />
           </div>
           <h3>Connected</h3>
-          <div className='clients_list'>
-            {
-              client.map((ele)=>{
-                return <Client key={ele.socket} client={ele.username}/>
-              })
-            }
+          <div className="clients_list">
+            {client.map((ele) => {
+              return <Client key={ele.socket} client={ele.username} />;
+            })}
           </div>
         </div>
-        <button className='btn copy_btn'>Copy Room ID</button>
-        <button className='btn leave_btn'>Leave</button>
+        <button className="btn copy_btn">Copy Room ID</button>
+        <button className="btn leave_btn">Leave</button>
       </div>
-      <div className='editor_wrap'>
-        <Editor/>
+      <div className="editor_wrap">
+        <Editor />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditorPage
-
+export default EditorPage;
